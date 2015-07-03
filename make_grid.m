1e4ab398eb5125ccfb4d88a86cd76bdfc240daba
mathwolf <denvermath@hotmail.com>
2015-07-02 18:33:52 -0600

    Create function to make gridpoints for unit circle

---
+++ make_grid.m
@@ -0,0 +1,48 @@
+function make_grid()
+%   Create a square grid of points mapping the interior of the domain
+%   of the problem.  Each point contains 0 or 1.
+
+%   First example: the unit circle.  Say that the grid is defined for all
+%   points 0, +/-  0.1, +/- 0.2, ...
+
+% total number of rows, columns
+h = 0.02;
+N = 101;
+
+grid = zeros(N,N);
+count_interior_points = 0;
+
+for i = 1:N
+    for j = 1:N
+        x = -1. + h * (i-1);
+        y = -1. + h * (j-1);
+        if x^2 + y^2 < 1
+           grid(i,j) = 1; 
+           count_interior_points = count_interior_points + 1;
+        end
+    end
+end
+
+% Create plot of points
+% disp(grid);
+disp(count_interior_points);
+
+% vector containing x-coordin
+X = zeros(count_interior_points, 1);
+Y = zeros(count_interior_points, 1);
+current_point = 1;
+for i = 1:N
+    for j = 1:N
+        if grid(i,j) == 1
+           X(current_point) = -1. + h * (i-1);
+           Y(current_point) = -1. + h * (j-1);
+           current_point = current_point + 1;
+        end
+    end
+end
+disp(X);
+disp(Y);
+scatter(X,Y);
+
+end
+
