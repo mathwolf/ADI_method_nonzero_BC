2a3133054cf97d7b98a140ab8e7645bae4d938c8
mathwolf <denvermath@hotmail.com>
2015-07-05 21:31:13 -0600

    Swap test function

--- f.m
+++ f.m
@@ -2,6 +2,8 @@ function z = f(x,y,t)
 % Forcing term for test function
 
 % z = -3 * cos(x^2 + y^2 + t) + (4*x^2 + 4*y^2) * sin(x^2 + y^2 + t);
-z = - exp(x+y+t);
+% z = - exp(x+y+t);
+z = - 12 * exp(3*x + 2*y + t);
+
 end
 
--- u.m
+++ u.m
@@ -2,7 +2,8 @@ function z = u(x,y,t)
 % Exact solution u(x,y,t) used as test function
 
 % z = sin(x^2 + y^2 + t);
-z = exp(x + y + t);
+% z = exp(x + y + t);
+z = exp(3*x + 2*y + t);
 
 end
 
