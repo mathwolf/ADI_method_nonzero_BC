58b9b9bc8b529a13f59dc14a783e969eed1ec3fa
mathwolf <denvermath@hotmail.com>
2016-02-06 09:52:41 -0700

    2D Dyakonov with and without perturbation

--- u.m
+++ u.m
@@ -1,14 +1,17 @@
 function z = u(x,y,t)
-% Exact solution u(x,y,t) used ffor testing
+% Exact solution u(x,y,t) used for testing
 
 % Constants used to select different test functions
+EXPONENT_0 = 0;
 EXPONENT_1 = 1;
 EXPONENT_2 = 2;
 TRIG = 3;
 POLY = 4;
 global test_solution
 
-if test_solution == TRIG
+if test_solution == EXPONENT_0
+    z = x * (1-x) * y * (1-y) * exp(x+y+t);
+elseif test_solution == TRIG
     z = 10 * cos(16*x^2 + 4*y^2 + t);
 elseif test_solution == EXPONENT_1
     z = exp(x + y + t);
