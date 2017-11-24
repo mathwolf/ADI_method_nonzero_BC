3b56bb466379dcd68a74ddac3e7a0ea7d1f9f12e
Bart Lantz <underhilllabs@gmail.com>
2017-11-23 20:22:14 -0700

    adds code indenting

--- u.m
+++ u.m
@@ -1,27 +1,27 @@
 function z = u(x,y,t)
 
-% Constants used to select different testing parameters
-EXPONENT_0 = 0;
-EXPONENT_1 = 1;
-EXPONENT_2 = 2;
-EXPONENT_3 = 3;
-TRIG = 4;
+    % Constants used to select different testing parameters
+    EXPONENT_0 = 0;
+    EXPONENT_1 = 1;
+    EXPONENT_2 = 2;
+    EXPONENT_3 = 3;
+    TRIG = 4;
 
-% Exact solution used as test function
-global test_solution
+    % Exact solution used as test function
+    global test_solution
 
-if test_solution == EXPONENT_0
-    z = x*(1-x)*y*(1-y)*exp(x+y+t);
-elseif test_solution == EXPONENT_1
-    z = exp(x+y+t);
-elseif test_solution == EXPONENT_2
-    z = exp(x + 2*y + 3*t);
-elseif test_solution == EXPONENT_3
-    z = exp(x*y*t);
-elseif test_solution == TRIG
-    z = 10 * cos(16*x^2+4*y^2+t);
-else
-    z = 0;
-end
+    if test_solution == EXPONENT_0
+        z = x*(1-x)*y*(1-y)*exp(x+y+t);
+    elseif test_solution == EXPONENT_1
+        z = exp(x+y+t);
+    elseif test_solution == EXPONENT_2
+        z = exp(x + 2*y + 3*t);
+    elseif test_solution == EXPONENT_3
+        z = exp(x*y*t);
+    elseif test_solution == TRIG
+        z = 10 * cos(16*x^2+4*y^2+t);
+    else
+        z = 0;
+    end
 end
 
